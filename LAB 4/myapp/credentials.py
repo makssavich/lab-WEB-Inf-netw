bd68c0e26b3475b4630fce54596daf5b4ec324bd
makssavich <161763396+makssavich@users.noreply.github.com>
2024-03-02 23:21:54 +0200

    Update credentials.py

--- LAB 4/myapp/credentials.py
+++ LAB 4/myapp/credentials.py
@@ -1,10 +1,10 @@
-TOKEN = '6639606450:AAExFtNXzYBK_YQAOaR2aIV2Tkr9__e3TzU'
+TOKEN = '***'
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/'
-URL = 'https://0c1b-195-66-79-237.ngrok-free.app/getpost/'
+URL = 'https:**.app/getpost/'
 
 HOSTDB='**************************************************'
 DBNAME='*****************'
 PORTDB=***********
 USERDB='avnadmin'
-PASSDB='AVNS_xXtQVHTV5sugciKFJ2c'
+PASSDB='AVNS_xXtQ****c'
 TIMEOUT=10
